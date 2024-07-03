1769871efab230ba76f92f4941e4e26512bf7b99
GongDonghai <donghai.gong@outlook.com>
2024-07-03 17:48:35 +0800

    Fix

---
+++ D365CrmSampleCode.Plugins/D365CrmSampleCode.Plugins.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("D365CrmSampleCode.Plugins.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("D365CrmSampleCode.Plugins.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44a9fae1-332c-456a-9ad6-31f07e3f09cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
